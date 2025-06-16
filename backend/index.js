c431986e7015d0d106b44367572379ef719f527e
tsirekidze22 <tsirekidze022@gmail.com>
2025-06-17 02:10:33 +0400

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,14 +6,13 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors());
 app.use(express.json());
 
 app.use(
   cors({
     origin: [
       "http://localhost:3000", // local
-      "https://your-frontend.vercel.app", // deployed
+      "https://country-tracker-api.vercel.app", // deployed
     ],
   })
 );
